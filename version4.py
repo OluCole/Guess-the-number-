3381376b0a376735b14f8e9017272e3d653f985a
OluCole <coleseyi19@gmail.com>
2024-11-01 08:44:24 -0400

    Update version4.py

--- version4.py
+++ version4.py
@@ -1,8 +1,7 @@
 import random as rd
 counter = 1
-mumber = random.randint(1,10)
 guesses = []
-
+mumber = random.randint(1,10)
 print("Lets play guess the number! I will have a number in mind from one  to ten.")
 print("When you think you know the number I have in mind, type it!")
 print(mumber)
