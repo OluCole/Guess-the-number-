3ebb16c4a075cee4af8c5a8cb44eb5abddb69462
OluCole <coleseyi19@gmail.com>
2024-11-04 08:10:59 -0500

    Update version4.py

--- version4.py
+++ version4.py
@@ -1,11 +1,21 @@
 import random as rd
+
 counter = 1
 guesses = []
-mumber = random.randint(1,10)
+mumber = rd.randint(1,10)
+
 print("Lets play guess the number! I will have a number in mind from one  to ten.")
 print("When you think you know the number I have in mind, type it!")
+
 print(mumber)
+
 while len(guesses) < 5:
-   mumber = int(input("Type the number you guessed!: "))
-   guesses.append(mumber)
-   print(guesses)
+   print(f"You have already guessed: {guesses}")
+   number = int(input("Type the number you guessed!: "))
+   guesses.append(number)
+
+   if mumber == number:
+      print("You win!")
+      break
+
+   counter += 1
